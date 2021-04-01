e39e0cd3a404cbd565b091d1498b9ea5e2d84015
Skenos Peniel <skenos3s@gmail.com>
2021-04-01 15:56:41 +0530

    Delete function implemented

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,6 +6,11 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
   render() {
     return (
       <table className="table">
@@ -20,11 +25,20 @@ class Movies extends Component {
         </thead>
         <tbody>
           {this.state.movies.map((movie) => (
-            <tr>
-              <td key={movie._id}>{movie.title}</td>
-              <td key={movie._id}>{movie.genre.name}</td>
-              <td key={movie._id}>{movie.numberInStock}</td>
-              <td key={movie._id}>{movie.dailyRentalRate}</td>
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                {" "}
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>{" "}
+              </td>
             </tr>
           ))}
         </tbody>
