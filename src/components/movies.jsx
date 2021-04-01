792ed6763d73c6cef1bf3083dc27ad39cdb5fddc
Skenos Peniel <skenos3s@gmail.com>
2021-04-01 15:49:20 +0530

    setting up project

---
+++ src/components/movies.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+export default Movies;
